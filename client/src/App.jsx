1cd09a4b0e122e8ac272ae9afcda2f4e6869259b
Taqueer Ali Tapya <tauqeertapya123@gmail.com>
2024-08-10 15:48:28 +0530

    added pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,25 @@
+import { Route, Routes } from "react-router-dom"
+
+import Home from './pages/Home'
+import About from './pages/About'
+import Dashboard from './pages/Dashboard'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Projects from './pages/Projects'
 
 function App() {
 
   return (
-    <div className="text-5xl text-center">MERN Blog</div>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </div>
   )
 }
 
