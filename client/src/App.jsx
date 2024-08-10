49ab39036fe662b7fad5d66468b6fdcc4bc8041b
Taqueer Ali Tapya <tauqeertapya123@gmail.com>
2024-08-11 04:05:31 +0530

    created header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 import Home from './pages/Home'
 import About from './pages/About'
@@ -6,11 +6,13 @@ import Dashboard from './pages/Dashboard'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Projects from './pages/Projects'
+import Header from "./components/Header"
 
 function App() {
 
   return (
-    <div>
+    <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
@@ -19,7 +21,7 @@ function App() {
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
-    </div>
+    </BrowserRouter>
   )
 }
 
